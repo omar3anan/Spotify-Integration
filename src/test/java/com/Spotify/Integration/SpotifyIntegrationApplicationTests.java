f6b4d7ac57650257f1b7ffa161552f51edeb4302
omar3anan <78151847+omar3anan@users.noreply.github.com>
2024-09-07 19:09:49 +0300

    Initial commit

---
+++ src/test/java/com/Spotify/Integration/SpotifyIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spotify.Integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
